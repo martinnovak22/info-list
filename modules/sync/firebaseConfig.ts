ffd9eaec7926c85882d521b46ef3e82821ebcbc7
Martin Novák <martin.novak@argo22.com>
2026-01-13 15:34:19 +0100

    chore: moved data to .env

--- modules/sync/firebaseConfig.ts
+++ modules/sync/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyANVlWQkbXsLfqdzqGhILNDGYxsnL775TM",
-    authDomain: "info-list-a83ad.firebaseapp.com",
-    databaseURL: "https://info-list-a83ad-default-rtdb.europe-west1.firebasedatabase.app/",
-    projectId: "info-list-a83ad",
-    storageBucket: "info-list-a83ad.firebasestorage.app",
-    messagingSenderId: "524667365610",
-    appId: "1:524667365610:web:2c7847ed6670591df1cf93"
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.EXPO_PUBLIC_FIREBASE_DATABASE_URL,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
